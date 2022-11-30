c01d7244bbaeb8edfabef8c49bd41832485c7a59
neil <neil.danson@gmail.com>
2022-11-30 18:14:33 +0000

    Menu layer now correct

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use bevy::{
     log::LogPlugin,
     prelude::*,
     render::{
-        mesh::shape::Cube,
         settings::{WgpuFeatures, WgpuSettings},
     },
     sprite::collide_aabb,
@@ -15,10 +14,21 @@ use rand::{thread_rng, Rng};
 const PLAYER_WIDTH: f32 = 34.0;
 const PLAYER_HEIGHT: f32 = 24.0;
 const PLAYER_FRAMES: usize = 4;
-const PLAYER_LAYER: f32 = 4.0;
 const PIPE_WIDTH: f32 = 52.0;
 const PIPE_HEIGHT: f32 = 320.0;
-const PIPE_POS : f32 = 250.0;
+const PIPE_POS : f32 = 225.0;
+const FLOOR_WIDTH : f32 = 336.0;
+const FLOOR_HEIGHT : f32 = 112.0;
+const FLOOR_POS : f32 = 200.0;
+const SCROLL_SPEED : f32 = 2.0;
+
+
+const MENU_LAYER : f32 = 6.0;
+const PLAYER_LAYER: f32 = 5.0;
+const PIPE_LAYER : f32 = 3.0;
+const FLOOR_LAYER : f32 = 4.0;
+const BACKGROUND_LAYER : f32 = 1.0;
+
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum GameState {
@@ -60,10 +70,16 @@ impl Default for GameOverEntity {
 struct Player;
 
 #[derive(Component)]
+struct Collidable(f32, f32);
+
+#[derive(Component)]
 struct Animation;
 
 #[derive(Component)]
-struct Obstacle;
+struct Pipe;
+
+#[derive(Component)]
+struct Floor;
 
 #[derive(Component)]
 struct Background;
@@ -91,7 +107,7 @@ struct AnimationTimer(Timer);
 //#[derive(Component, Deref, DerefMut)]
 //struct Random(rand_chacha::ChaCha8Rng);
 
-fn global_setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn global_setup(mut commands: Commands, asset_server: Res<AssetServer>, audio: Res<Audio>) {
     let camera = Camera2dBundle {
         camera: Camera { ..default() },
         projection: OrthographicProjection {
@@ -108,7 +124,12 @@ fn global_setup(mut commands: Commands, asset_server: Res<AssetServer>) {
 
     let die: Handle<AudioSource> = asset_server.load("die.ogg");
     commands.insert_resource(DieSoundEffect(die));
-    //commands.insert_resource(Random(rand::thread_rng()));
+
+    audio.play_with_settings(
+            asset_server.load("Music.ogg"),
+            PlaybackSettings::LOOP.with_volume(0.75),
+        );
+    
 }
 
 //Menu
@@ -117,7 +138,7 @@ fn menu_setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     let sprite = SpriteBundle {
         texture: asset_server.load("message.png"),
         transform: Transform {
-            translation: Vec3::new(0.0, 0.0, 3.0),
+            translation: Vec3::new(0.0, 0.0, MENU_LAYER),
             ..Transform::default()
         },
         ..default()
@@ -155,22 +176,22 @@ fn gameover_keyboard_input(keys: Res<Input<KeyCode>>, mut commands: Commands) {
     }
 }
 
-fn obstacle_setup<TEntity : Default + Component>(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn pipe_setup<TEntity : Default + Component>(mut commands: Commands, asset_server: Res<AssetServer>) {
     for i in 2..6 {
         let sprite = SpriteBundle {
             transform: Transform {
-                translation: Vec3::new(i as f32 * 200.0, -PIPE_POS, 1.0),
+                translation: Vec3::new(i as f32 * 200.0, -PIPE_POS, PIPE_LAYER),
                 ..Transform::default()
             },
             texture: asset_server.load("pipe-green.png"),
             ..default()
         };
 
-        commands.spawn(sprite).insert(Obstacle).insert(create::<TEntity>());
+        commands.spawn(sprite).insert(Pipe).insert(Collidable(PIPE_WIDTH, PIPE_HEIGHT)).insert(create::<TEntity>());
 
         let sprite = SpriteBundle {
             transform: Transform {
-                translation: Vec3::new(i as f32 * 200.0, PIPE_POS, 1.0),
+                translation: Vec3::new(i as f32 * 200.0, PIPE_POS, PIPE_LAYER),
                 ..Transform::default()
             },
             texture: asset_server.load("pipe-green.png"),
@@ -181,7 +202,22 @@ fn obstacle_setup<TEntity : Default + Component>(mut commands: Commands, asset_s
             ..default()
         };
 
-        commands.spawn(sprite).insert(Obstacle).insert(create::<TEntity>());
+        commands.spawn(sprite).insert(Pipe).insert(Collidable(PIPE_WIDTH, PIPE_HEIGHT)).insert(create::<TEntity>());
+    }
+}
+
+fn floor_setup<TEntity : Default + Component>(mut commands: Commands, asset_server: Res<AssetServer>) {
+    for i in 0..10 {
+        let sprite = SpriteBundle {
+            transform: Transform {
+                translation: Vec3::new(i as f32 * FLOOR_WIDTH, -FLOOR_POS, FLOOR_LAYER),
+                ..Transform::default()
+            },
+            texture: asset_server.load("base.png"),
+            ..default()
+        };
+
+        commands.spawn(sprite).insert(Floor).insert(Collidable(FLOOR_WIDTH, FLOOR_HEIGHT)).insert(create::<TEntity>());
     }
 }
 
@@ -189,7 +225,7 @@ fn background_setup<TEntity : Default + Component>(mut commands: Commands, asset
     for i in -3..3 {
         let sprite = SpriteBundle {
             transform: Transform {
-                translation: Vec3::new(i as f32 * 288.0, 0.0, 0.0),
+                translation: Vec3::new(i as f32 * 288.0, 0.0, BACKGROUND_LAYER),
                 ..Transform::default()
             },
             texture: asset_server.load("background-day.png"),
@@ -221,16 +257,16 @@ fn rainbow_fart_setup(
 
     let effect1 = effects.add(
         EffectAsset {
-            name: "emit:rate".to_string(),
+            //name: "emit:rate".to_string(),
             capacity: 32768,
-            spawner: Spawner::rate(1000.0.into()),
+            spawner: Spawner::rate(100.0.into()),
             ..Default::default()
         }
-        .init(PositionSphereModifier {
-            center: Vec3::ZERO,
-            radius: 2.,
-            dimension: ShapeDimension::Surface,
+        .init(PositionCircleModifier {
+            radius: 0.05,
             speed: 6.0.into(),
+            dimension: ShapeDimension::Surface,
+            ..Default::default()
         })
         .update(AccelModifier {
             accel: Vec3::new(-200., -3., 100.),
@@ -243,15 +279,18 @@ fn rainbow_fart_setup(
         }),
     );
 
-    let cube = meshes.add(Mesh::from(Cube { size: 1.0 }));
-    let mat = materials.add(Color::PURPLE.into());
+    let cube = meshes.add(Mesh::from(shape::Quad {
+        size: Vec2::splat(2.0),
+        ..Default::default()
+    }));
 
+    let mat = materials.add(Color::PURPLE.into());
     commands
         .spawn((
-            Name::new("emit:rate"),
+            //Name::new("emit:rate"),
             ParticleEffectBundle {
-                effect: ParticleEffect::new(effect1),
-                transform: Transform::from_translation(Vec3::new(-10., 0., 2.)),
+                effect: ParticleEffect::new(effect1).with_z_layer_2d(Some(FLOOR_LAYER)),
+                transform: Transform::from_translation(Vec3::new(-10., 0., 5.)),
                 ..Default::default()
             },
         ))
@@ -263,7 +302,6 @@ fn rainbow_fart_setup(
                     material: mat.clone(),
                     ..Default::default()
                 },
-                Name::new("source"),
             ));
         })
         .insert(InGameEntity);
@@ -328,6 +366,11 @@ fn gravity(mut query: Query<(&mut Velocity, &mut Transform)>) {
     transform.translation.y += velocity.y;
 }
 
+fn rotate(mut query: Query<(&Velocity, &mut Transform)>) {
+    let (velocity, mut transform) = query.single_mut();
+    *transform = transform.with_rotation(Quat::from_rotation_z(f32::clamp(velocity.y / 2.0, -1.0, 1.0)));
+}
+
 fn animation(
     time: Res<Time>,
     texture_atlases: Res<Assets<TextureAtlas>>,
@@ -356,7 +399,7 @@ fn scroll_background(mut pipes: Query<&mut Transform, With<Background>>) {
     }
 }
 
-fn move_pipes(mut pipes: Query<&mut Transform, With<Obstacle>>) {
+fn move_pipes(mut pipes: Query<&mut Transform, With<Pipe>>) {
     let mut  rng = thread_rng();
     let random = rng.gen::<f32>() - 1.0;
     for mut t in pipes.iter_mut() {
@@ -368,7 +411,17 @@ fn move_pipes(mut pipes: Query<&mut Transform, With<Obstacle>>) {
                 t.translation.y = -PIPE_POS + 20.0 * random;
             }
         } else {
-            t.translation.x -= 2.0;
+            t.translation.x -= SCROLL_SPEED;
+        }
+    }
+}
+
+fn move_floor(mut floor: Query<&mut Transform, With<Floor>>) {
+    for mut t in floor.iter_mut() {
+        if t.translation.x <= -FLOOR_WIDTH * 2.0 {
+            t.translation.x = FLOOR_WIDTH * 2.0;
+        } else {
+            t.translation.x -= SCROLL_SPEED;
         }
     }
 }
@@ -386,16 +439,16 @@ fn check_collisions(
     mut commands: Commands,
     audio: Res<Audio>,
     die: Res<DieSoundEffect>,
-    player: Query<(&Transform, Entity), (With<Player>, Without<Obstacle>)>,
-    obstacles: Query<&mut Transform, (With<Obstacle>, Without<Player>)>,
+    player: Query<(&Transform, Entity), (With<Player>, Without<Collidable>)>,
+    obstacles: Query<(&mut Transform, &Collidable), Without<Player>>,
 ) {
     let (player, player_entity) = player.single();
     let player_pos = player.translation;
     let player_size = Vec2::new(PLAYER_WIDTH, PLAYER_HEIGHT);
 
-    for obstacle in obstacles.iter() {
+    for (obstacle, col) in obstacles.iter() {
         let obstacle_pos = obstacle.translation;
-        let obstacle_size = Vec2::new(PIPE_WIDTH, PIPE_HEIGHT);
+        let obstacle_size = Vec2::new(col.0, col.1);
         let collision = collide_aabb::collide(player_pos, player_size, obstacle_pos, obstacle_size);
         match collision {
             Some(_) => {
@@ -439,19 +492,21 @@ fn main() {
                     filter: "bevy_hanabi=warn,spawn=trace".to_string(),
                 }),
         )
-        //.add_plugin(HanabiPlugin)
+        .add_plugin(HanabiPlugin)
         .add_startup_system(global_setup)
         .add_loopless_state(GameState::MainMenu)
 
         .add_enter_system(GameState::MainMenu, menu_setup)
-        .add_enter_system(GameState::MainMenu, obstacle_setup::<MenuEntity>)
+        .add_enter_system(GameState::MainMenu, pipe_setup::<MenuEntity>)
+        .add_enter_system(GameState::MainMenu, floor_setup::<MenuEntity>)
         .add_enter_system(GameState::MainMenu, background_setup::<MenuEntity>)
         .add_exit_system(GameState::MainMenu, cleanup::<MenuEntity>)
 
         .add_exit_system(GameState::InGame, cleanup::<InGameEntity>)
         .add_enter_system(GameState::InGame, player_setup)
-        //.add_enter_system(GameState::InGame, rainbow_fart_setup)
-        .add_enter_system(GameState::InGame, obstacle_setup::<InGameEntity>)
+        .add_enter_system(GameState::InGame, rainbow_fart_setup)
+        .add_enter_system(GameState::InGame, pipe_setup::<InGameEntity>)
+        .add_enter_system(GameState::InGame, floor_setup::<InGameEntity>)
         .add_enter_system(GameState::InGame, background_setup::<InGameEntity>)
 
         .add_enter_system(GameState::GameOver, gameover_setup)
@@ -462,6 +517,7 @@ fn main() {
                 .run_in_state(GameState::MainMenu)
                 .with_system(scroll_background)
                 .with_system(move_pipes)
+                .with_system(move_floor)
                 .with_system(menu_keyboard_input)
                 .into()
         )
@@ -470,10 +526,12 @@ fn main() {
             ConditionSet::new()
                 .run_in_state(GameState::InGame)
                 .with_system(animation)
-                //.with_system(move_particles)
+                .with_system(move_particles)
                 .with_system(scroll_background)
                 .with_system(move_pipes)
+                .with_system(move_floor)
                 .with_system(gravity)
+                .with_system(rotate)
                 .with_system(keyboard_input)
                 .with_system(check_collisions)
                 .with_system(play_flap)
